4630a09cc22bc30b737ec7ae233d974310832f11
vy2000eng <vy2000eng@gmail.com>
2024-06-25 19:59:29 -0400

    currently clicking on the section below updates the section above in flashcardsetviewcontroller

--- Studify/Resources/delegates/FlashCardSetViewControllerDelegate.swift
+++ Studify/Resources/delegates/FlashCardSetViewControllerDelegate.swift
@@ -9,6 +9,18 @@ import Foundation
 import UIKit
 
 extension FlashCardSetViewController: UICollectionViewDelegate{
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        if indexPath.section == 1{
+            let newIndexPath = IndexPath(item: indexPath.row, section: 0)
+            DispatchQueue.main.async{
+                self.collectionView.performBatchUpdates {
+                    self.collectionView.scrollToItem(at: newIndexPath, at: .centeredHorizontally, animated: true)
+                }
+                
+            }
+            
+        }
+    }
     
     
 }
